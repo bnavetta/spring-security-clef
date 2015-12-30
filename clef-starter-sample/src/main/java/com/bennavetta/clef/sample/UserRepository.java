2015168e29d04a382e1623add73345bafd691684
Ben Navetta <ben.navetta@gmail.com>
2015-12-29 22:59:28 -0500

    remove old sample code

--- clef-starter-sample/src/main/java/com/bennavetta/clef/sample/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bennavetta.clef.sample;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String>
-{
-    public User findByClefId(String clefId);
-}
